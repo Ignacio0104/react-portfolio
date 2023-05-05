4e9b0c35f62d782915a9013b8e96b692a3929c15
Ignacio Smirlian <ignacio.smirlian@globant.com>
2023-05-05 15:45:40 -0300

    download CV and background image OK

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import ProfilePic from "../assets/images/profile-pic-small.jpg";
+import "../styles/homeComponent/Home.css";
+import BackgroundImage from "../assets/images/background-mountains.jpg";
+import CvFile from "../assets/documents/IgnacioSmirlian-CV2023.pdf";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="home-container">
+        <div className="information-container">
+          <div className="image-container">
+            <img src={ProfilePic} alt="profile"></img>
+          </div>
+          <div className="description-container">
+            <p>Hello, I'm </p>
+            <h3>Ignacio Smirlian</h3>
+            <p>
+              A passionate <span>Frontend developer </span>dedicated to creating
+              interactive web experiences, skilled in <span>HTML</span>,
+              <span> CSS</span>,<span> JavaScript</span>, and
+              <span> React</span>, among other technologies.
+            </p>
+            <div className="button-container">
+              <div className="btn-cv-container">
+                <a
+                  className="btn-cv"
+                  href={CvFile}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Download cv
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
